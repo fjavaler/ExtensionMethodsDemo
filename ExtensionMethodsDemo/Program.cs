1ccaffb22124e989423f8657e2f211a6ba398e5d
Frederick Javalera <frederickjavalera@gmail.com>
2020-09-11 14:08:10 -0600

    Init commit.

---
+++ ExtensionMethodsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExtensionMethodsDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string strName = "fred";
+
+      //extension method added to string class
+      string result = strName.ChangeFirstLetterCase();
+
+      //behind the scenes it gets converted into this
+      //string result = StringHelper.ChangeFirstLetterCase(strName);
+
+      Console.WriteLine(result);
+    }
+  }
+}
